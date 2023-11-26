0b3ccc3d709b9c65a94a2f6da140376e39cd6136
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-26 19:50:39 +0300

    Added HTTPStatus code for custom error pages

--- algosolve/pages/views.py
+++ algosolve/pages/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 
+from http import HTTPStatus
+
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    return render(
+        request,
+        'pages/404.html',
+        status=HTTPStatus.NOT_FOUND.value)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
+    return render(
+        request,
+        'pages/403csrf.html',
+        status=HTTPStatus.FORBIDDEN.value)
 
 
 def server_error(request):
-    return render(request, 'pages/500.html', status=500)
+    return render(
+        request,
+        'pages/500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR.value)
